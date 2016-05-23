60a84b2fa513526ce943a521f6f6550a997a1bc8
dhcrain <dhcrain@gmail.com>
2016-05-23 13:39:13 -0400

    1st task comleted

--- comprehensions.py
+++ comprehensions.py
@@ -1,2 +1,11 @@
-# Remove all vowels from this sentence List Comprehensions are the Greatest!
+# Remove all vowels from this sentence, List Comprehensions are the Greatest!
+sentence = "List Comprehensions are the Greatest!"
+vowels = "aeiouAEIOU"
+no_vowels = ''.join([letter for letter in sentence if not letter in vowels])
+print(no_vowels)
+
+# Create a list of Water Temps for each day the data set below.
+
+with open("dataset") as opened_file:
+    data = opened_file.read()
 
