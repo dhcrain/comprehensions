79d89490e8cfb38672c1e02cc3a07c8f4bc8114f
dhcrain <dhcrain@gmail.com>
2016-05-23 14:26:55 -0400

    Create a list of Water Temps for each day

--- comprehensions.py
+++ comprehensions.py
@@ -5,7 +5,18 @@ no_vowels = ''.join([letter for letter in sentence if not letter in vowels])
 print(no_vowels)
 
 # Create a list of Water Temps for each day the data set below.
+import csv
 
-with open("dataset") as opened_file:
-    data = opened_file.read()
+# with open("dataset.csv") as opened_file:
+#     data_list = list(csv.reader(opened_file))
+# print((data_list))
 
+temps = []
+with open('dataset.csv', 'r') as csvfile:
+    csvreader = (csv.reader(csvfile))
+    # This skips the first row of the CSV file.
+    next(csvreader)
+    # for row in csvreader:
+    #     temps.append(row[4])
+    temps = [row[4] for row in csvreader]
+print(temps)
